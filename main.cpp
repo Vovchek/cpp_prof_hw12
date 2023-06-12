9635c3ad28627c1c128af3e907cf37b9d05a3814
Vovchek <39309092+Vovchek@users.noreply.github.com>
2023-06-12 20:01:34 +0300

    fix: setup project version in CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
   {
     if (argc != 2)
     {
-      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+      std::cerr << "Usage: join_server <port>\n";
       return 1;
     }
 
